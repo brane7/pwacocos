d5ee1486f1e32f940e4422f20590aac1885c75a3
jongkyu <brane7@naver.com>
2025-08-20 14:58:41 +0900

    no message

--- index.29192.js
+++ index.29192.js
@@ -1,4 +1,4 @@
-System.register(["./application.3bbe9.js"], function (_export, _context) {
+System.register(["./application.3cb1c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
